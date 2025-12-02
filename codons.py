2186736ed1a194338907956133dd37af22a45cd8
korendn-sys <korendn@gmail.com>
2025-12-02 17:42:44 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,19 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
+    file = open(file_path)
+    rows = file.readlines()
+    file.close()
 
+    row = rows[0]
+    clean = row.strip()
+    parts = clean.split('\t')
+    print(clean)
+    print(parts)
 
+    codon_dict = {}
+    for row in rows:
+        parts = row.strip().split('\t')
+        codon = parts[0]
+        aa = parts[2]
+        codon_dict[codon] = aa
+
+    return codon_dict
